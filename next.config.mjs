9766a39ca51a8f988fe17debe06be8bcd22a82ce
chaun <demetricwhitlock@live.com>
2026-01-21 11:04:46 -0600

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  // Static export so you can upload the built files to almost any host (cPanel, S3, etc.)
-  output: 'export',
-  trailingSlash: true,
-  images: { unoptimized: true }
+  // output: "export",
+  // ...
 };
-
 export default nextConfig;
